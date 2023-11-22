7731eb152f7513a9e910337f132d156e1bc6ece5
Hoàng Diệu Linh <136173788+dieulinh0809@users.noreply.github.com>
2023-11-22 16:40:02 +0700

    Update lthdtbai18.cpp

--- lthdtbai18.cpp
+++ lthdtbai18.cpp
@@ -9,43 +9,45 @@ class PhanSo{
     private :
         float a,b,c,d;
     public:
-        void nhap();
-        int tinhTong();
+            int TinhTong();
+            int RutGon();
 
 };
-
 //==Chuong trinh chinh==//
 int main(){
     //Tao 1 lop dtg
-    PhanSo ps;
-    //Tuong tac
-    ps.nhap();
-    ps.tinhTong();
+        PhanSo ps;
+     //Tuong tac
+    ps.Nhap();
+    ps.TinhTong();
+    ps.RutGon();
 
     cout<<endl;
     return 0;
 }
 //=Dinh nghia ham ==//
-
-void PhanSo::nhap(){
-    cout<<"Moi nhap vao a,b(a/b) va c,d(c/d): ";
-    cin>>a>>b>>c>>d;
+void PhanSo::Nhap(){
+        cout<<"Moi nhap vao a,b(a/b) va c,d(c/d): ";
+        cin>>a>>b>>c>>d;
 }
-int  PhanSo::tinhTong(){
-    int e=a*d+b*c,f=b*d,ucln;
-    printf("Phan so chua rut gon la : %d/%d",e,f);
-    int temp = e, tg =f;
-    int r = e%f;
-    while(r!=0){
-        e=f;
-        f=r;
-        r=e%f;
-        if(r==0){
-             ucln =f;
-        }
-    }
-    cout<<endl;
-    printf("Phan so chua rut gon la : %d/%d",temp/ucln,tg/ucln);
 
+int  PhanSo::TinhTong(){
+        int e=a*d+b*c,f=b*d,ucln;
+        printf("Phan so chua rut gon la : %d/%d",e,f);
+}
 
+int PhanSo::RutGon(){
+        int e=a*d+b*c,f=b*d,ucln;
+        int temp = e, tg =f;
+        int r = e%f;
+        while(r!=0){
+            e=f;
+            f=r;
+            r=e%f;
+            if(r==0){
+                ucln =f;
+              }
+           }
+         cout<<endl;
+         printf("Phan so rut gon la : %d/%d",temp/ucln,tg/ucln);
 }
