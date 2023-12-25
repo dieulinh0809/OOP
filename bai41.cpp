73e0ea78b29da23348baa8c008a10e0c2d98a2f5
Hoàng Diệu Linh <136173788+dieulinh0809@users.noreply.github.com>
2023-12-25 10:57:51 +0700

    Create bai41.cpp

---
+++ bai41.cpp
@@ -0,0 +1,95 @@
+/*
+Bài 41(lthdtbai41.cpp): Thời điểm là một loại ngày tháng và cũng là một loại thời gian
+nhưng có thêm địa điểm. Ngày tháng có ngày, tháng, năm. Thời gian có giờ và phút. Nhập vào một thời điểm,
+đưa ra địa điểm, ngày và thời gian của thời điểm đó ở dạng Địa điểm, ngày dd/mm/yy - h:mm.
+Ví dụ: Hà Nội, ngày 22/04/19 - 9:30
+*/
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+using namespace std;
+
+class NgayThang
+{
+    private:
+        int dd,mm,yy;
+    public:
+        void nhap();
+        void hien();
+};
+
+class ThoiGian
+{
+    private:
+        int gio,phut;
+    public:
+        void nhap();
+        void hien();
+};
+
+class ThoiDiem:public NgayThang,ThoiGian
+{
+    private:
+        char diaDiem[100];
+    public:
+        void nhap();
+        void hien();
+
+};
+
+int main()
+{
+    ThoiDiem td;
+    td.nhap();
+    td.hien();
+    cout<<endl;
+    return 0;
+}
+
+//==Dinh nghia ham
+void NgayThang::nhap()
+{
+    cout<<"Nhap vao ngay: ";
+    cin>>dd;
+    cout<<"Nhap vao thang : ";
+    cin>>mm;
+    cout<<"Nhap vao nam : ";
+    cin>>yy;
+}
+
+void NgayThang::hien()
+{
+    cout<<dd<<"/"<<mm<<"/"<<yy;
+}
+
+void ThoiGian::nhap()
+{
+    cout<<"Nhap vao gio: ";
+    cin>>gio;
+    cout<<"Nhap vao phut : ";
+    cin>>phut;
+}
+
+void ThoiGian::hien()
+{
+    cout<<gio<<":"<<phut;
+}
+
+void ThoiDiem::nhap()
+{
+    NgayThang::nhap();
+    ThoiGian::nhap();
+    cout<<"Nhap vao dia diem : ";
+    //cin.getline(diaDiem,100);
+    cin.ignore();
+    cin.getline(diaDiem,100);
+    getchar();
+}
+
+void ThoiDiem::hien()
+{
+    cout<<diaDiem<<",";
+    NgayThang::hien();
+    cout<<" - ";
+    ThoiGian::hien();
+}
